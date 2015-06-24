c0f2440783b74832f83631ffd98920cc133df879
Zoheyr Doctor <zoheyr@gmail.com>
2015-06-24 10:37:42 -0500

    added all data files and preliminary KN field code

---
+++ des_io.py
@@ -0,0 +1,98 @@
+import numpy as np
+from itertools import ifilter
+
+dtypes = {
+    'CID':'|S15',
+    'MJD':float,
+    'Tobs':float,
+    'FLUXCAL':float,
+    'FLUXCAL_ERR':float,
+    'FLUXCALERR':float,
+    'DATAFLAG':int,
+    'BAND':'|S1',
+    'CHI2':float,
+    'IFIT':float,
+    'FLT':'|S1',
+    'FIELD':'|S2',
+    'PHOTFLAG':int,
+    'PHOTPROB':float,
+    'PSF':float,
+    'ZPFLUX':float,
+    'TEMPLERR':float,
+    'ZPT':float,
+    'SNR':float,
+    'MAG':float,
+    'MAGERR':float,
+    'SIM_MAGOBS':float,
+    'RA':float,
+    'DEC':float,
+    'HOST_ZPHOT':float,
+    'N_NITE_OBS':int,
+    'N_NITE_TRIG':int,
+    'g1_MJD':float,
+    'g1_FLUX':float,
+    'g1_FLUXERR':float,
+    'r1_MJD':float,
+    'r1_FLUX':float,
+    'r1_FLUXERR':float,
+    'i1_MJD':float,
+    'i1_FLUX':float,
+    'i1_FLUXERR':float,
+    'z1_MJD':float,
+    'z1_FLUX':float,
+    'z1_FLUXERR':float,
+    'g2_MJD':float,
+    'g2_FLUX':float,
+    'g2_FLUXERR':float,
+    'r2_MJD':float,
+    'r2_FLUX':float,
+    'r2_FLUXERR':float,
+    'i2_MJD':float,
+    'i2_FLUX':float,
+    'i2_FLUXERR':float,
+    'z2_MJD':float,
+    'z2_FLUX':float,
+    'z2_FLUXERR':float,
+    'g3_MJD':float,
+    'g3_FLUX':float,
+    'g3_FLUXERR':float,
+    'r3_MJD':float,
+    'r3_FLUX':float,
+    'r3_FLUXERR':float,
+    'i3_MJD':float,
+    'i3_FLUX':float,
+    'i3_FLUXERR':float,
+    'z3_MJD':float,
+    'z3_FLUX':float,
+    'z3_FLUXERR':float,
+    'g4_MJD':float,
+    'g4_FLUX':float,
+    'g4_FLUXERR':float,
+    'r4_MJD':float,
+    'r4_FLUX':float,
+    'r4_FLUXERR':float,
+    'i4_MJD':float,
+    'i4_FLUX':float,
+    'i4_FLUXERR':float,
+    'z4_MJD':float,
+    'z4_FLUX':float,
+    'z4_FLUXERR':float}
+
+def parse_observations(infile):
+    with open(infile, 'r') as inp:
+        # Get the column headers
+        header = inp.readline().split()
+        while 'VARLIST:' not in header:
+            header = inp.readline().split()
+
+        # Prepare data type for struc. array
+        params = header
+        cols = [i for i in range(len(params)) if params[i] in dtypes.keys()]
+        dtype = [(params[col], dtypes[params[col]]) for col in cols]
+
+        # Filter out anything that isn't an observation
+        filtered_inp = ifilter(lambda line: line.startswith('OBS:'), inp)
+        obs = np.genfromtxt(filtered_inp, usecols=cols, dtype=dtype)
+
+    return obs 
+
---
+++ des_utils.py
@@ -0,0 +1,109 @@
+import numpy as np
+import des_io
+
+
+def extract_colors(infiles):
+    nobjects = len(infiles)
+
+    triggers = np.empty(nobjects, dtype='bool')
+    colors = np.zeros(nobjects)
+    ifluxes = np.zeros(nobjects)
+    for i, infile in enumerate(infiles):
+        # get a list with all the values in the data table
+        obs = des_io.parse_observations(infile)
+
+        # Separate deep and shallow fields
+        deep_sel = deepfield(obs)
+        deep = obs[deep_sel]
+        shallow = obs[~deep_sel]
+
+        # Look just at shallow fields for now
+        # make a list of all the nites there were observations and make a nitedictlist
+        nitelist = np.unique(shallow['MJD'].astype('int'))
+
+        zobs, zMJD, zflux = obsinband(shallow, 'z') # identify whether there was a z observation on a nite and get its MJD
+        iobs, iMJD, iflux = obsinband(shallow, 'i') # identify whether there was an i observation on a nite and get its MJD
+
+        zdet = zobs == 2
+        idet = iobs == 2
+        zsel, isel = common_nites(zMJD, iMJD)
+        trig_flags = zdet[zsel] & idet[isel]
+        trig = np.any(trig_flags)
+        triggers[i] = trig
+        if trig:
+            zflux1 = zflux[zdet & zsel][0]
+            iflux1 = iflux[idet & isel][0]
+            colors[i] = -2.5*(np.log10(iflux1)-np.log10(zflux1))
+            ifluxes[i] = iflux[iobs > 0][-1] - iflux[iobs > 0][0]
+    return triggers, colors, ifluxes
+
+
+def deepfield(obs):
+    sel = (obs['FIELD'] == 'X3') | (obs['FIELD'] == 'C3')
+    return sel
+
+def obsinband(obslist, band='i'):
+    # returns list with 0's for no observations in that band on that nite,
+    # 1's for if there is a near field observation in that band on that nite with all cuts/detects met, 2 if 
+    # there is a deep field obs on that nite with cuts/detects met.  3 and 4 signal that there was a 
+    # near or deep (resp.) observation that night but with no requirement on those observations passing
+    # cuts and detects.  Also returns obsMJD which is a list of the MJD's of those observations in that band
+    # -----------------------------------
+    obs = obslist[obslist['FLT'] == band]
+
+    nitelist = np.unique(obs['MJD'].astype('int'))
+    nnites = len(nitelist)
+
+    obsband = np.zeros(nnites, dtype='int')
+    obsflux = np.empty(nnites)
+
+    for x, nite in enumerate(nitelist):
+        nite_obs = obs[obs['MJD'].astype('int') == nite]
+        det = detected(nite_obs)
+        passed = within_cuts(nite_obs)
+        if np.any(det & passed):
+            sel = det & passed
+            obsband[x] = 2
+            obsflux[x] = np.mean(nite_obs[sel]['FLUXCAL'])
+        else:
+            obsband[x] = 1
+            obsflux[x] = np.mean(nite_obs['FLUXCAL'])
+    return (obsband, nitelist, obsflux)
+
+
+def common_nites(nitelist1, nitelist2):
+    cnites = np.array([nite for nite in nitelist1 if np.any(nitelist2 == nite)])
+
+    sel1 = np.array([True if nite in cnites else False for nite in nitelist1], dtype='bool')
+    sel2 = np.array([True if nite in cnites else False for nite in nitelist2], dtype='bool')
+    return sel1, sel2
+    
+def detected(obs):
+    # given an observation dictionary, check if that observation counts as a detection
+    # ----------------------------------------------------------
+    sel = obs['PHOTFLAG'] > 1
+
+    try:
+        sel = sel & (obs['PHOTPROB'] >= 0.5)
+    except ValueError:
+        pass
+
+    return sel 
+
+def within_cuts(obs, zp_lower=30.5, zp_upper=34.0, zp_fwhm_lower=-.5, zp_fwhm_upper=2.0):
+    # given an observation dictionary, check if that observation passes the cuts defined here
+    # ------------------------------------------------------
+    zpname = 'ZPFLUX'
+    try:
+        obs[zpname]
+    except ValueError:
+        zpname = 'ZPT'
+
+    zp_sel = (obs[zpname] > zp_lower) & (obs[zpname] < zp_upper)
+
+    try:
+        psf_sel = (obs['PSF'] > zp_fwhm_lower) & (obs['PSF'] < zp_fwhm_upper)
+    except ValueError:
+        psf_sel = np.ones(len(obs), dtype='bool')
+
+    return zp_sel & psf_sel 
